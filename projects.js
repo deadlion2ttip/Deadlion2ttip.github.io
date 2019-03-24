9bc1bd420302e89f58a9510a7c952df9549f0856
deadion2ttip <39771562+deadion2ttip@users.noreply.github.com>
2019-03-23 21:41:02 -0400

    Updated About and project descriptions. Cleaned up some obvious code smells.

--- projects.js
+++ projects.js
@@ -1,18 +1,18 @@
-let projectsHash = {
+const projectsHash = {
     'Zorkington': {
-        'description' : "A text based adventure coded in JavaScript during week 3 at Burlington Code Academy. Using hashes and state machines, the game keeps track of actions available in each location and tracks a player inventory.",
+        'description' : 'A text based adventure coded in JavaScript during week 3 at Burlington Code Academy. Using hashes and state machines, the game keeps track of actions available in each location and tracks a player inventory.',
         'ghLink' : 'https://github.com/djmorosini/zorkington',
         'projectLink': 'zorkdata/zorkington.html',
         'image': 'images/zorkingtonthumbnailorig.jpg',
     },
     'GreenUp': {
-        'description' : 'A web hosted dashboard designed at the behest of Code for BTV and Green Up Vermont. I assembled a team of classmates and used the agile process to develop a dashboard for the display of data collected by a previously created phone app. The dashboard utilizes OOP to organize and distribute data pulled from a Firebase Database, and displays it using leaflet.js and charts.js. The web app is currently live on firebase hosting and is expected to be public facing by the end of the month.',
+        'description' : 'A web app dashboard designed at the behest of Code for BTV and Green Up Vermont. I assembled a team of classmates and used the agile process to develop a dashboard for the display of data collected by a previously created phone app. The dashboard utilizes OOP to organize and distribute data pulled from a Firebase Database, and displays it using leaflet.js and charts.js. The web app is currently live on firebase hosting and is being extended by Code for BTV',
         'ghLink' : 'https://github.com/smsvt99/greenupDashboard',
         'projectLink' : 'https://greenupvermont-de02b.firebaseapp.com',
         'image' : 'images/greenup.jpg'
     },
     'Yelpington': {
-        'description': "A restaurant review site using Leaflet maps, OpenStreetMaps API, and using AJAX to load in data from JSON files. The map and index restaurant list construction are done on the fly using data from JSON files in the root folder. Hosted using Heroku. Created week 5 at Burlington Code academy.",
+        'description': 'A restaurant review site using Leaflet maps, OpenStreetMaps API, and using AJAX to load in data from JSON files. The map and index restaurant list construction are done on the fly using data from JSON files in the root folder. Hosted using Heroku. Created week 5 at Burlington Code academy.',
         'ghLink': 'https://github.com/deadlion2ttip/yelpington',
         'projectLink': 'https://limitless-thicket-25617.herokuapp.com/',
         'image': 'images/yelpingtontnorig.jpg',
@@ -24,7 +24,7 @@ let projectsHash = {
         'image': 'images/geo-vermont.jpg'
     },
     'chat': {
-        'description': 'An object oriented chat server and GUI hosted on Heroku. It utilizes HTTP, OOJS, and URL encoding and decoding to create a functioning public facing chat client with multiple chatrooms. Created week 8 at Burlington Code Academy.',
+        'description': 'A chat server utilizing HTTP, OOJS, and URL encoding and decoding to create a functioning public facing chat client with multiple chatrooms. Created week 8 at Burlington Code Academy.',
         'ghLink': 'https://github.com/mfelix5/simple-server',
         'projectLink': 'https://radiant-brook-30764.herokuapp.com/',
         'image': 'images/chattn.jpg'
